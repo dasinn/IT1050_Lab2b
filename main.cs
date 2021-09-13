7dd769966e674e74f9896ac2460caf90d85870a3
David Wildasinn <89878913+dasinn@users.noreply.github.com>
2021-09-13 17:19:34 +0000

    changed double variables to integers

--- main.cs
+++ main.cs
@@ -3,24 +3,24 @@ using System;
 class Program {
   public static void Main (string[] args) {
     Console.WriteLine ("Enter number 1 of 2:");
-    double num1 = Convert.ToDouble(Console.ReadLine());
+    int num1 = Convert.ToInt32(Console.ReadLine());
 
     Console.WriteLine ("Enter number 2 of 2:");
-    double num2 = Convert.ToDouble(Console.ReadLine());
+    int num2 = Convert.ToInt32(Console.ReadLine());
 
-    double sum = num1 + num2;
-    Console.WriteLine ("The sum of numer 1 and number 2 is " + sum);
+    int sum = num1 + num2;
+    Console.WriteLine ("The sum of number 1 and number 2 is " + sum);
 
-    double diff = num1 - num2;
+    int diff = num1 - num2;
     Console.WriteLine ("The difference of number 1 and number 2 is " + diff);
 
-    double product = num1 * num2;
+    int product = num1 * num2;
     Console.WriteLine ("The product of number 1 and number 2 is " + product);
 
-    double quotient = num1 / num2;
+    int quotient = num1 / num2;
     Console.WriteLine ("The quotient of number 1 and number 2 is " + quotient);
 
-    double remainder = num1 % num2;
+    int remainder = num1 % num2;
     Console.WriteLine ("The remainder between number 1 and number 2 is " + remainder);
   }
 }
